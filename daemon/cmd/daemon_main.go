make-bin/cilium

commit 9654cb4562d78f4a03d14ac879c500631e2f7f6a

daemon: Fatal if host fw is enabled w/o remote node identities
The host firewall currently only works if remote node identities are
distinct from the host identity (i.e., REMOTE_NODE_ID is supported).

It's not clear if this restriction can be removed in the future,
especially once #12345 is merged. In particular, #12345 will redirect
packets from local endpoints through the host device if the destination
security ID is HOST_ID. If the HOST_ID includes remote nodes, then this
redirection is likely to result in packet drops.

Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/daemon_main.go
@@ -1130,8 +1130,13 @@ func initEnv(cmd *cobra.Command) {
 	initClockSourceOption()
 	initSockmapOption()
 
-	if option.Config.EnableHostFirewall && option.Config.EnableIPSec {
-		log.Fatal("IPSec cannot be used with the host firewall.")
+	if option.Config.EnableHostFirewall {
+		if option.Config.EnableIPSec {
+			log.Fatal("IPSec cannot be used with the host firewall.")
+		}
+		if !option.Config.EnableRemoteNodeIdentity {
+			log.Fatalf("%s must be enabled to use the host firewall.", option.EnableRemoteNodeIdentity)
+		}
 	}
 
 	// If there is one device specified, use it to derive better default

test/k8sT/Policies.go
@@ -1186,9 +1186,12 @@ var _ = Describe("K8sPolicyTest", func() {
 					By("Reconfiguring Cilium to disable remote-node identity")
 					newCfg := map[string]string{
 						"global.remoteNodeIdentity": "false",
+						"global.hostFirewall":       "false",
 					}
 					for k, v := range daemonCfg {
-						newCfg[k] = v
+						if _, ok := newCfg[k]; !ok {
+							newCfg[k] = v
+						}
 					}
 					RedeployCilium(kubectl, ciliumFilename, newCfg)
 				})

commit 7ffe452859709706752daf6f689f837064044898

daemon: Allow host fw w/o the remote-node identity
In 9654cb4, the Cilium agent was made to fatal if the host firewall was
enabled while the remote-node identity was disabled. This was based on
tests using existing policy examples during which the connectivity check
was failing.

It turns out the host firewall works fine without the remote-node
identity. The policy just needs to be adapted to take that into account,
by using the node CIDR in place of the remote-node identity. This commit
thus reverts 9654cb4.

Fixes: 9654cb4 ("daemon: Fatal if host fw is enabled w/o remote node identities")
Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/daemon_main.go
@@ -1142,9 +1142,6 @@ func initEnv(cmd *cobra.Command) {
 		if option.Config.EnableIPSec {
 			log.Fatal("IPSec cannot be used with the host firewall.")
 		}
-		if !option.Config.EnableRemoteNodeIdentity {
-			log.Fatalf("%s must be enabled to use the host firewall.", option.EnableRemoteNodeIdentity)
-		}
 		if option.Config.EnableEndpointRoutes {
 			log.Fatalf("%s cannot be used with the host firewall. Packets must be routed through the host device.", option.EnableEndpointRoutes)
 		}

test/k8sT/Policies.go
@@ -1353,7 +1353,6 @@ var _ = Describe("K8sPolicyTest", func() {
 					RedeployCiliumWithMerge(kubectl, ciliumFilename, daemonCfg,
 						map[string]string{
 							"global.remoteNodeIdentity": "false",
-							"global.hostFirewall":       "false",
 						})
 				})
 